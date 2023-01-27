844fdeeb5a6676259495ae7e21ec974e6ff86388
Cameron <cam.avarley@gmail.com>
2023-01-26 23:01:36 -0500

    Fix Search

--- components/layout/BlogHeader.tsx
+++ components/layout/BlogHeader.tsx
@@ -1,6 +1,8 @@
 import Search from "../../search.json";
 import { useState } from "react";
 import Link from 'next/link';
+import { navigate } from "@reach/router"
+
 
 export default function BlogHeader({ title }) {
     const [search, setSearch] = useState()
@@ -8,16 +10,25 @@ export default function BlogHeader({ title }) {
 
         setSearch(value.target.value)
     }
+
     let qval = Object.values(Object.values({ pathname: '/Search', query: { q: search?.toLowerCase() } })[1])[0]
+
+    function handleKeyPress(target) {
+        // I'm guessing you have value stored in state
+        if (target.charCode == 13) {
+            console.log(Object.values({ qval })[0])
+            window.location.href = `/Search?q=${Object.values({ qval })[0]}`;
+        }
+    }
     return (
         <div className="bg-white m-2 rounded-lg flex flex-col md:flex-row overflow-hidden justify-between md:items-center py-3 px-6" >
             <div><strong>{title}</strong></div>
-            <form className="flex max-md:mt-2" action={"/Search?q=" + qval} method="post">
-                <input onChange={findSerach} className="mr-2 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0" type="text" placeholder="Enter search term..." aria-label="Enter search term..." aria-describedby="button-search" />
+            <div className="flex max-md:mt-2">
+                <input onKeyPress={e => handleKeyPress(e)} onChange={findSerach} className="mr-2 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0" type="text" placeholder="Enter search term..." aria-label="Enter search term..." aria-describedby="button-search" />
                 <Link className="min-w-[42px] flex justify-center items-center aspect-square text-white rounded-md bg-neutral-600 border-transparent" href={{ pathname: '/Search', query: { q: search?.toLowerCase() } }}>
                     GO
                 </Link>
-            </form>
+            </div>
         </div >
     )
 }
