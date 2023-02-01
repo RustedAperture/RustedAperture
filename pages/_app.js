bb6236a1dc5aad6edeb7165b184729eb5070995d
Cameron <cam.avarley@gmail.com>
2023-02-01 05:20:45 -0500

    Add Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,13 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 import Layout from '../components/layout/Layout';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
