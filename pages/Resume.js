4dd4306beded3c6ce76605cfff6652f98517cfe9
Cameron Varley <cam.avarley@gmail.com>
2023-09-21 16:57:49 -0400

    Update Resume

--- pages/Resume.js
+++ pages/Resume.js
@@ -11,7 +11,7 @@ export default function Resume() {
         </Head>
         <div id="top" className="gap-y-8 xl:sticky top-8 max-xl:self-stretch drop-shadow-md flex flex-row flex-wrap xl:flex-col items-stretch sm:min-w-[20rem] bg-white dark:bg-zinc-800 rounded-lg p-6 m-2">
             <div>
-                <p>Productive, efficient and proven fast learning indivual. Currently working as an event and product support specialist with Givergy. My current position puts me in a position to support our operations teams. My skills are focused in programming (web & application), photography, photo/video editing and Linux/Unix system administration.</p>
+                <p>Self-motivated and results-oriented IT professional with a proven track record of success in event and product support, programming, and Linux/Unix system administration. Fast learner with a passion for new challenges and a commitment to continuous improvement. Expertise in supporting operations teams to achieve their goals.</p>
             </div>
             <div className="flex gap-x-3">
                 <span><FontAwesomeIcon icon={faLocationPin}/> Mississauga, Canada</span>
@@ -56,12 +56,12 @@ export default function Resume() {
                 <div>
                     <p className="text-xl font-bold">Event and Product Support Specialist <span className="font-normal text-base"><FontAwesomeIcon icon={faCircle} size="2xs"/> June 2022</span></p>
                     <p>Givergy</p>
-                    <p>Focused on the event delivery and supporting our North American on site staff to ensure a smooth event process. Quality assurance of our platform through testing. Responding and coordination with our development team on bugs.</p>
+                    <p>Event delivery and support for North American on-site staff, ensuring a smooth event process. Platform quality assurance through testing and bug coordination with the development team.</p>
                 </div>
                 <div>
                     <p className="text-xl font-bold">Global Support Agent <span className="font-normal text-base"><FontAwesomeIcon icon={faCircle} size="2xs"/> Aug 2021 - June 2022</span></p>
                     <p>Givergy</p>
-                    <p>Providing technical support for a SaS product at a global scale. Handling and priotizing cases and tickets from customers in many regions including Hong Kong, Australia, North America, and the UK.</p>
+                    <p>Provide global technical support for a SaaS product, managing and prioritizing customer cases and tickets from Hong Kong, Australia, North America, and the UK.</p>
                 </div>
                 <div>
                     <p className="text-xl font-bold">Tech Manager <span className="font-normal text-base"><FontAwesomeIcon icon={faCircle} size="2xs"/> May 2019 - June 2023</span></p>
