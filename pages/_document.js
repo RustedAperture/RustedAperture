5365f241ee1ce9950725d8057f0a69438f7dbdc7
Cameron Varley <cam.avarley@gmail.com>
2023-09-20 20:56:04 -0400

    Fix padding for small device screen

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" /> 
       </Head>
-        <body className='font-medium grid dark:bg-zinc-600 bg-slate-300 p-3 md:p-6 text-base text-zinc-800 dark:text-zinc-50 -min-h-screen min-w-screen place-items-center content-start'>
+        <body className='font-medium grid dark:bg-zinc-600 bg-slate-300 md:p-6 text-base text-zinc-800 dark:text-zinc-50 -min-h-screen min-w-screen place-items-center content-start'>
           <Main />
           <NextScript />
         </body>
