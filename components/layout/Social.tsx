718773ee3e4e8553fa52396c4500b9bb6408f856
Cameron Varley <cameron.varley@givergy.com>
2024-12-09 00:06:45 -0500

    update bluesky color

--- components/layout/Social.tsx
+++ components/layout/Social.tsx
@@ -9,7 +9,7 @@ export default function Social() {
     return (
         <div className="xl:sticky top-[346px] xl:max-w-[50rem] max-xl:self-stretch drop-shadow-md flex flex-row flex-wrap items-stretch justify-center ml-1.5 mr-1.5">
             <a
-                className="min-w-[48px] text-2xl text-center text-blue-500 p-3 m-0.5"
+                className="min-w-[48px] text-2xl text-center text-sky-500 p-3 m-0.5"
                 href="https://bsky.app/profile/rustedaperture.xyz"
                 target="_blank"
                 rel="noreferrer"
