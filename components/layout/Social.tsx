b1ff167a9dd3bbd2eaac9713c547f4da4a469fa5
Cameron Varley <cam.avarley@gmail.com>
2025-09-10 03:28:10 -0400

    update icons to square

--- components/layout/Social.tsx
+++ components/layout/Social.tsx
@@ -1,20 +1,29 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faGithub,
-    faBluesky,
-    faLinkedin,
+    faSquareXTwitter,
+    faSquareGithub,
+    faSquareBluesky,
+    faSquareLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
 
 export default function Social() {
     return (
         <div className="xl:sticky top-[346px] xl:max-w-[50rem] max-xl:self-stretch drop-shadow-md flex flex-row flex-wrap items-stretch justify-center ml-1.5 mr-1.5">
             <a
+                className="min-w-[48px] text-2xl text-center text-zinc-900 p-3 m-0.5"
+                href="https://x.com/rustedaperture"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <FontAwesomeIcon icon={faSquareXTwitter} />
+            </a>
+            <a
                 className="min-w-[48px] text-2xl text-center text-sky-500 p-3 m-0.5"
                 href="https://bsky.app/profile/rustedaperture.xyz"
                 target="_blank"
                 rel="noreferrer"
             >
-                <FontAwesomeIcon icon={faBluesky} />
+                <FontAwesomeIcon icon={faSquareBluesky} />
             </a>
             <a
                 className="min-w-[48px] text-2xl text-center text-gray-600 dark:text-gray-300 p-3 m-0.5"
@@ -22,7 +31,7 @@ export default function Social() {
                 target="_blank"
                 rel="noreferrer"
             >
-                <FontAwesomeIcon icon={faGithub} />
+                <FontAwesomeIcon icon={faSquareGithub} />
             </a>
             <a
                 className="min-w-[48px] text-2xl text-center text-blue-500 p-3 m-0.5"
@@ -30,7 +39,7 @@ export default function Social() {
                 target="_blank"
                 rel="noreferrer"
             >
-                <FontAwesomeIcon icon={faLinkedin} />
+                <FontAwesomeIcon icon={faSquareLinkedin} />
             </a>
         </div>
     );
