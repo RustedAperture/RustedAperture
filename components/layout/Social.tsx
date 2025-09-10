c0277daee8d1076427da76453374fe1ab076b35f
Cameron Varley <cam.avarley@gmail.com>
2025-09-10 03:28:53 -0400

    make icons larger

--- components/layout/Social.tsx
+++ components/layout/Social.tsx
@@ -10,7 +10,7 @@ export default function Social() {
     return (
         <div className="xl:sticky top-[346px] xl:max-w-[50rem] max-xl:self-stretch drop-shadow-md flex flex-row flex-wrap items-stretch justify-center ml-1.5 mr-1.5">
             <a
-                className="min-w-[48px] text-2xl text-center text-zinc-900 p-3 m-0.5"
+                className="min-w-[48px] text-3xl text-center text-zinc-900 p-3 m-0.5"
                 href="https://x.com/rustedaperture"
                 target="_blank"
                 rel="noreferrer"
@@ -18,7 +18,7 @@ export default function Social() {
                 <FontAwesomeIcon icon={faSquareXTwitter} />
             </a>
             <a
-                className="min-w-[48px] text-2xl text-center text-sky-500 p-3 m-0.5"
+                className="min-w-[48px] text-3xl text-center text-sky-500 p-3 m-0.5"
                 href="https://bsky.app/profile/rustedaperture.xyz"
                 target="_blank"
                 rel="noreferrer"
@@ -26,7 +26,7 @@ export default function Social() {
                 <FontAwesomeIcon icon={faSquareBluesky} />
             </a>
             <a
-                className="min-w-[48px] text-2xl text-center text-gray-600 dark:text-gray-300 p-3 m-0.5"
+                className="min-w-[48px] text-3xl text-center text-gray-600 dark:text-gray-300 p-3 m-0.5"
                 href="https://github.com/RustedAperture"
                 target="_blank"
                 rel="noreferrer"
@@ -34,7 +34,7 @@ export default function Social() {
                 <FontAwesomeIcon icon={faSquareGithub} />
             </a>
             <a
-                className="min-w-[48px] text-2xl text-center text-blue-500 p-3 m-0.5"
+                className="min-w-[48px] text-3xl text-center text-blue-500 p-3 m-0.5"
                 href="https://www.linkedin.com/in/cameron-varley-687368136/"
                 target="_blank"
                 rel="noreferrer"
